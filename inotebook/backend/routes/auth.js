571879c1d904856bba7e860d588fb79edc605c0d
Anuj <anujverma3553@gmail.com>
2023-10-26 14:59:46 +0530

    Added Hash Passwords using bcryptjs in NodeJs

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "Harryisagood$boy"
 // Create a User using : POST "/api/auth/createuser". No login required
 router.post('/createuser', [
     // Name must be at least 3 chars long
@@ -11,7 +14,7 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     // Password must be at least 5 chars long
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
-], async(req, res) => {
+], async (req, res) => {
     // If there are errors, return Bad request and the erors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -24,16 +27,21 @@ router.post('/createuser', [
     if(user){
         return res.status(400).json({error : "Sorry a user with this email already exists"})
     }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    // Create a new user
     user = await User.create({
         name: req.body.name,
+        password: secPass,
         email: req.body.email,
-        password: req.body.password,
     })
-    
-    // .then(user => res.json(user))
-    // .catch(err => {console.log(err)
-    // res.json({error : 'Please unique value for email', message : err.message})})
-    res.json(user);
+    const data = {
+        user :{
+            id : user.id,
+        }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken});
     }catch(err){
         console.log(err.message);
         res.status(500).send("Some Error Occured")
