fde94c41eb25d0b241f1924229ae30a6a02d10ab
Anuj <anujverma3553@gmail.com>
2023-10-26 21:12:18 +0530

    Fetching all notes & Adding a Note

--- inotebook/backend/models/Note.js
+++ inotebook/backend/models/Note.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
 mongoose.connect('mongodb://0.0.0.0:27017/inotebook');
 
 const NotesSchema = new Schema({
+    user:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+    },
     title:{
         type : String,
         required : true
