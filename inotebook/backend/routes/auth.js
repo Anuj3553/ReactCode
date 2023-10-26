bfe318b6302b999d6b87dfbb1af6dba9ce312ae1
Anuj <anujverma3553@gmail.com>
2023-10-26 13:40:58 +0530

    Create ThunderClient Collection to manage request

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,28 +3,41 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-// Create a User using : POST "/api/auth". Does'nt require Auth
-router.post('/', [
+// Create a User using : POST "/api/auth/createuser". No login required
+router.post('/createuser', [
     // Name must be at least 3 chars long
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     // Email must be an email
     body('email', 'Enter a valid email').isEmail(),
     // Password must be at least 5 chars long
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
-], (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
+], async(req, res) => {
+    // If there are errors, return Bad request and the erors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
+    // Check whether the user with this mail exists already
+    try{
+    let user = await User.findOne({email : req.body.email});
+    if(user){
+        return res.status(400).json({error : "Sorry a user with this email already exists"})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error : 'Please unique value for email', message : err.message})})
+    })
+    
+    // .then(user => res.json(user))
+    // .catch(err => {console.log(err)
+    // res.json({error : 'Please unique value for email', message : err.message})})
+    res.json(user);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("Some Error Occured")
+    }
 })
 
 module.exports = router;
