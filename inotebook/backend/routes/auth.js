1d4a1e73ec26d1bffdf5c70ce6e2a027867f8991
Anuj <anujverma3553@gmail.com>
2023-10-26 15:47:05 +0530

    Creating Login Endpoint & sending auth token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = "Harryisagood$boy"
+const JWT_SECRET = "Anujisagood$boy";
 // Create a User using : POST "/api/auth/createuser". No login required
 router.post('/createuser', [
     // Name must be at least 3 chars long
@@ -22,30 +22,67 @@ router.post('/createuser', [
     }
 
     // Check whether the user with this mail exists already
-    try{
-    let user = await User.findOne({email : req.body.email});
-    if(user){
-        return res.status(400).json({error : "Sorry a user with this email already exists"})
-    }
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(req.body.password, salt);
-    // Create a new user
-    user = await User.create({
-        name: req.body.name,
-        password: secPass,
-        email: req.body.email,
-    })
-    const data = {
-        user :{
-            id : user.id,
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
-    }
-    const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({authtoken});
-    }catch(err){
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        })
+        const data = {
+            user: {
+                id: user.id,
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken });
+    } catch (err) {
         console.log(err.message);
         res.status(500).send("Some Error Occured")
     }
 })
 
+// Create a User using : POST "/api/auth/login". No login required
+
+
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password can not be blank').exists(),
+], async (req, res) => {
+    // If there are errors, return Bad request and the erors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if (!user){
+            return res.status(400).json({error: "Please try to login with correct credentials"})
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please try to login with correct credentials"})
+        }
+
+        const data = {
+            user: {
+                id: user.id 
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Internal Server Error")
+    }
+})
 module.exports = router;
