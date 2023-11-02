2ede03527f9b7e568ea80cdd0796c520225606c6
Anuj <anujverma3553@gmail.com>
2023-11-02 12:22:37 +0530

    Accessing State in Redux

--- reduxapp/src/components/Navbar.js
+++ reduxapp/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 const Navbar = () => {
+    const amount = useSelector(state => state.amount)
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -19,7 +20,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                         <div>
-                            <button disabled={true} className="btn-primary btn">Your Balance : 10000</button>
+                            <button disabled={true} className="btn-primary btn">Your Balance : {amount}</button>
                         </div>
                     </div>
                 </div>
